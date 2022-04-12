2866f54851a95af1196ff49ef8fb52eadabbbbf5
estarmalkia <estarmalkia@gmail.com>
2022-04-12 07:53:58 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "putchar.h"
 /**
-* main - prints the string "Holberton" from a character array.
+* main - prints the string "Putchar" from a character array.
 * Return: 0 on success.
 */
 int main(void)
 {
-char c[] = "Holberton";
+char c[] = "Putchar";
 int i;
 for (i = 0; i < 9; i++)
 {
